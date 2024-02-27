a3dba729c2a1ae26686bcf87f0a933afb107ea69
kkjh9909 <kkjh990957@gmail.com>
2024-02-27 21:46:58 +0900

    인덱스 번호 추가

--- src/main/kotlin/com/example/quote/entity/Quote.kt
+++ src/main/kotlin/com/example/quote/entity/Quote.kt
@@ -13,7 +13,7 @@ class Quote {
     @Id
     @GeneratedValue(generator = "uuid2")
     @GenericGenerator(name="uuid2", strategy = "uuid2")
-    @Column(columnDefinition = "BINARY(16)")
+    @Column(columnDefinition = "VARCHAR(36)")
     var id: String = UUID.randomUUID().toString()
 
     lateinit var content: String
